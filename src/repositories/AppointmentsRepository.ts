5efa9fbfe1442db85b6ebdcbf41b3bf0083f4387
Manoel Gomes <manoelgomes53@gmail.com>
2020-04-20 12:03:02 -0300

    added on database connection

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,38 +1,17 @@
+import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
-import { isEqual } from 'date-fns';
 
-//Data Transfer Protocol
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public create(data: CreateAppointmentDTO): Appointment {
-    const { provider, date } = data;
-
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-    return appointment;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(ap =>
-      isEqual(ap.date, date),
-    );
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: {
+        date,
+      },
+    });
 
     return findAppointment || null;
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
   }
 }
 
