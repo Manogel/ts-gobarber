f21a13e4056644381d871160dbaabad37eb90935
Manogel <manoelgomes53@gmail.com>
2020-04-25 20:35:39 -0300

    ux input

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
@@ -10,6 +15,21 @@ export const Container = styled.div`
 
   display: flex;
   align-items: center;
+  transition: color 0.2s;
+  transition: border-color 0.2s;
+
+  ${({ isFocused }) =>
+    isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${({ isFilled }) =>
+    isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     flex: 1;
