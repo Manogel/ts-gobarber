fdabfb9535d54d536df4f775f2d7f1e8a72a083a
Manogel <manoelgomes53@gmail.com>
2020-04-26 18:21:40 -0300

    style guide

---
+++ mobile/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text> App </Text>
+    </View>
+  );
+};
+
+export default App;
